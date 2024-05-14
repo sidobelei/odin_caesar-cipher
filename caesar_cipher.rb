891c6ec5474b286c7dc7d033b47157672f24f9d3
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-14 13:21:32 -0700

    Add caesar_cipher function

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,27 @@
 def caesar_cipher (string, shift_factor)
-    #split array into characters
-    #map a shift_factor
-    #account for shifts that go past letters
-    #return new string
+    old_string = string.split("")
+    modified_string = old_string.map do |char|
+        ascii_char = char.ord
+        if (65..90).include?(ascii_char) || (97..122).include?(ascii_char)
+            if (65..90).include?(ascii_char)
+                if ascii_char + shift_factor > 90
+                    new_shift = shift_factor - (90 - ascii_char)
+                    ascii_char = 64 + new_shift
+                else
+                    ascii_char += shift_factor
+                end
+            else
+                if ascii_char + shift_factor > 122
+                    new_shift = shift_factor - (122 - ascii_char)
+                    ascii_char = 96 + new_shift
+                else
+                    ascii_char += shift_factor
+                end
+            end
+        end
+        ascii_char.chr
+    end
+    puts modified_string.join
 end
+
+caesar_cipher("What a string!", 29)
