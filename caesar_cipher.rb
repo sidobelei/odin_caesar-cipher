8f07b31c8fbfeda6a20cce4637216618198fe570
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-08-30 14:50:52 -0700

    Refactor and add normalize_shift_factor method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,7 @@
 def caesar_cipher (string, shift_factor)
     old_string = verify_string(string)
     return unless old_string
-    if shift_factor > 26 
-        shift_factor = shift_factor % 26
-    end
+    shift_factor = normalize_shift_factor(shift_factor)
     modified_string = old_string.map do |char|
         ascii_char = char.ord
         if (65..90).include?(ascii_char) || (97..122).include?(ascii_char)
@@ -31,6 +29,13 @@ end
 def verify_string(string)
     string.split("") if string.is_a?(String)
 end 
+
+def normalize_shift_factor(shift_factor)
+    if shift_factor > 26 
+        shift_factor = shift_factor % 26
+    end
+    return shift_factor
+end
 
 #Uncomment below to see output and change parameters
 #caesar_cipher("What a string!", 5)
