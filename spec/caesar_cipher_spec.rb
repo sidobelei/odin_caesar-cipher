8fc6cabe707541a7c203bcd40c2efa71d4d791d9
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-08-30 14:44:15 -0700

    Add tests for normalize_shift_factor method

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -13,3 +13,23 @@ describe '#verify_string' do
     end
   end
 end
+
+describe '#normalize_shift_factor' do
+  context 'when the shift factor is less than 26' do
+    it 'returns the same shift factor' do
+      expect(normalize_shift_factor(15)).to eq (15)
+    end
+  end
+  
+  context 'when the shift is equal to 26' do
+    it 'returns the a shift factor of 26' do
+      expect(normalize_shift_factor(26)).to eq(26)
+    end
+  end
+  
+  context 'when the shift factor is more than 26' do
+    it 'returns a normalized shift factor' do
+      expect(normalize_shift_factor(100)).to eq(22)
+    end
+  end
+end
