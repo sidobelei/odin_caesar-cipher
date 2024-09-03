cf6bb5bfe8258a66580754253ec2d1b445448852
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-09-03 16:06:51 -0700

    Add tests for caesar_cipher function

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -57,3 +57,39 @@ describe 'modify_string' do
     end
   end
 end
+
+describe '#caesar_cipher' do
+  let(:string) { 'Hello World!' }
+  let(:non_string) { 3453 }
+  let(:shift_factor) { 4 }
+  let(:string_array) { ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'] }
+  
+  context 'when given a string and shift_factor' do
+    it 'calls #verify_string' do
+      expect(self).to receive(:verify_string).with(string)
+      caesar_cipher(string, shift_factor)
+    end
+
+    it 'calls #normalize_shift_factor' do
+      expect(self).to receive(:normalize_shift_factor).with(shift_factor)
+      caesar_cipher(string, shift_factor)
+    end
+
+    it 'calls #modify_string' do
+      expect(self).to receive(:modify_string).with(string_array, shift_factor)
+      caesar_cipher(string, shift_factor)
+    end
+  end
+
+  context 'when not given a string and given a shift factor' do
+    it 'will return nil' do
+      expect(caesar_cipher(non_string, shift_factor)).to be_nil
+    end
+  end
+
+  context 'when given a string and not given a shift factor' do
+    it 'will return nil' do
+      expect(caesar_cipher(string, nil)).to be_nil
+    end
+  end
+end
